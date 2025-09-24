fa63693f0fdac7742453cacf824b23b638b03bf5
Barthromew Mufaya <barthromewraps@gmail.com>
2025-09-24 18:06:47 +0200

    Refactor navigation to use Dashboard page and remove unused Sign Up page

---
+++ lib/pages/dashboard.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({super.key});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          // Header (always visible, no fade)
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.w),
+                child: Text(
+                  "MESMTF",
+                  style: TextStyle(
+                    fontSize: 40,
+                    color: Color(0xFF009688),
+                    fontFamily: "BalooPaajiR",
+                  ),
+                ),
+              ),
+              SizedBox(width: 700.w),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Chatbot",
+                  style: TextStyle(
+                    fontFamily: "Clarendon",
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  "About Us",
+                  style: TextStyle(
+                    fontFamily: "Clarendon",
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Contact",
+                  style: TextStyle(
+                    fontFamily: "Clarendon",
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
