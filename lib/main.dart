f6db093b3086037be87079911856fa989410d177
Barthromew Mufaya <barthromewraps@gmail.com>
2025-09-25 20:54:14 +0200

    Refactor imports and clean up unused code in chatbot and dashboard pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nusthackationwebsite/pages/chatbot.dart';
 //import 'package:nusthackationwebsite/pages/chatbot.dart';
 import 'package:nusthackationwebsite/pages/dashboard.dart';
 //import 'package:nusthackationwebsite/pages/dashboard.dart';
