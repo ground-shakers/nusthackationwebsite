632a7c102f6a83d53758da92e87eb8643d5a3065
Barthromew Mufaya <barthromewraps@gmail.com>
2025-09-26 19:42:28 +0200

    refactor: Remove unused imports from main, contactus, signinpage, and signuppage files

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:nusthackationwebsite/authchecker.dart';
-import 'package:nusthackationwebsite/pages/landingpage.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/pages/contactus.dart
+++ lib/pages/contactus.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:nusthackationwebsite/const/textfield.dart';
 import 'package:nusthackationwebsite/pages/chatbot.dart';
 import 'package:nusthackationwebsite/pages/dashboard.dart';
